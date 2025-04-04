1179a47df4021f56f1524f897730d1e3e8ab7d9e
jayasuryapaduru <jayasuryapanduru@gmail.com>
2025-04-04 17:20:20 +0530

    Files Added

---
+++ SimpleCalculator/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package SimpleCalculator;
+
+public interface SimpleCalculator {
+		
+	public int addition();
+	public int subraction();
+	public int multiplication();
+	public double divison();
+	
+	
+	
+	
+}
